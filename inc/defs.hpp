c7ddbdd891d6460d3a1dd83a5daaccb75af51d0a
GruelingPine185 <adrian.bostic24@gmail.com>
2022-02-15 22:37:03 -0500

    typedefs for `pos_pair` and `dim_pair`

---
+++ inc/defs.hpp
@@ -0,0 +1,17 @@
+#ifndef DEFS_HPP
+#define DEFS_HPP
+
+
+// position pair
+typedef struct {
+    int x;  // horizontal offset
+    int y;  // vertical offset
+} pos_pair;
+
+// dimension pair
+typedef struct {
+    int w;  // width
+    int h;  // height
+} dim_pair;
+
+#endif // DEFS_HPP
