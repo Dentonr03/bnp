21f0932b4c934caf20050f257efe37173b3ff421
GruelingPine185 <adrian.bostic24@gmail.com>
2022-02-16 21:09:05 -0500

    updated spacing

--- src/main.cpp
+++ src/main.cpp
@@ -14,8 +14,8 @@ int main() {
     // setup
     noecho();
     Menu main_menu("Bits 'N Pieces");
-    main_menu.set_dim(getmaxx(game_win)/2, getmaxy(game_win)/2);
-    main_menu.set_pos(getmaxx(game_win)/4, getmaxy(game_win)/4);
+    main_menu.set_dim(getmaxx(game_win) / 2, getmaxy(game_win) / 2);
+    main_menu.set_pos(getmaxx(game_win) / 4, getmaxy(game_win) / 4);
     main_menu.init();
     main_menu.render();
 
