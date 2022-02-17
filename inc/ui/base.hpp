cd0abefba065f7527be172b47c3843ca4607c456
GruelingPine185 <adrian.bostic24@gmail.com>
2022-02-16 19:43:36 -0500

    added comments

--- inc/ui/base.hpp
+++ inc/ui/base.hpp
@@ -8,9 +8,13 @@
 class Base {
 public:
     Base();
+    /** sets the internal position pair with the given coordinates */
     void set_pos(const int __x, const int __y);
+    /** returns the position pair */
     pos_pair get_pos() const;
+    /** sets the internal dimension pair with the given width and height */
     void set_dim(const int __w, const int __h);
+    /** returns the dimension pair */
     dim_pair get_dim() const;
 private:
     pos_pair _pos;
