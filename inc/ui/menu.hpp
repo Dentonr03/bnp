6921d20ae49be27c2f8dcef570084074754e4251
GruelingPine185 <adrian.bostic24@gmail.com>
2022-02-17 07:24:08 -0500

    added comments

--- inc/ui/menu.hpp
+++ inc/ui/menu.hpp
@@ -9,10 +9,15 @@ class Menu: public Base {
 public:
     Menu(const char* __title);
     Menu();
+    /** returns 1 if the title is being used 0 otherwise */
     bool using_title();
+    /** sets the menu title to the given string */
     void set_title(const char* __title);
+    /** returns the menu title */
     const char* get_title() const;
+    /** returns the menu title coordinate pair */
     pos_pair get_title_pos() const;
+    /** handles redrawing. Returns 1 on success and 0 otherwise */
     bool update();
 private:
     text_pair _title;
