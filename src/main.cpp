75f453a3fb78b21974415d9d52d52bed742b0547
GruelingPine185 <adrian.bostic24@gmail.com>
2022-02-16 21:07:54 -0500

    added basic main menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <ncurses.h>
 
+#include "menu.hpp"
+
+
+// todo: error checking and handling for ncurses functions
 
 int main() {
     WINDOW* game_win = initscr();
@@ -9,6 +13,11 @@ int main() {
 
     // setup
     noecho();
+    Menu main_menu("Bits 'N Pieces");
+    main_menu.set_dim(getmaxx(game_win)/2, getmaxy(game_win)/2);
+    main_menu.set_pos(getmaxx(game_win)/4, getmaxy(game_win)/4);
+    main_menu.init();
+    main_menu.render();
 
     // do stuff
     getch();
