0b7656af7a826d5dd6228463ef1cd219edcf5eab
GruelingPine185 <adrian.bostic24@gmail.com>
2022-02-16 21:26:40 -0500

    defined macros for positioning

--- inc/shared/defs.hpp
+++ inc/shared/defs.hpp
@@ -5,6 +5,13 @@
 #define STRINGIFY(_data)    #_data
 #define TO_STR(_data)       STRINGIFY(_data)
 
+// positioning macros U = upper, L = lower
+#define U2LF(_x)    (_x / 2)
+#define U3RD(_x)    (_x / 3)
+#define L3RD(_x)    ((2 * _x) / 3)
+#define U4TH(_x)    (_x / 4)
+#define L4TH(_x)    ((_x * 3) / 4)
+
 
 // position pair
 typedef struct {
